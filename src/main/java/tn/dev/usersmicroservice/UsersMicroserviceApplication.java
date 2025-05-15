02740eada7df855fd7309e62ddd172cda1a4c63f
Thomas Noël <thomas.noel.31@gmail.com>
2025-05-15 23:38:32 +0200

    Updating user entities and create address entitie

--- src/main/java/tn/dev/usersmicroservice/UsersMicroserviceApplication.java
+++ src/main/java/tn/dev/usersmicroservice/UsersMicroserviceApplication.java
@@ -5,12 +5,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import tn.dev.usersmicroservice.entities.Address;
 import tn.dev.usersmicroservice.entities.Role;
 import tn.dev.usersmicroservice.entities.User;
 import tn.dev.usersmicroservice.services.UserService;
 
+import java.time.LocalDate;
+
 @SpringBootApplication
+@EnableJpaAuditing
 public class UsersMicroserviceApplication {
 
     public static void main(String[] args) {
@@ -26,10 +31,54 @@ public class UsersMicroserviceApplication {
         userService.createRole(new Role(null, "ADMIN"));
         userService.createRole(new Role(null, "USER"));
         userService.createRole(new Role(null, "ANONYMOUS"));
-        //ajouter les users
-        userService.createUser(new User(null, "admin", "admin", true, null));
-        userService.createUser(new User(null, "yapadlez", "password", true, null));
-        userService.createUser(new User(null, "johndoe", "password", true, null));
+        // Add users
+        User admin = User.builder()
+                .firstname("Super")
+                .lastname("Admin")
+                .username("admin")
+                .password("admin")
+                .email("admin@franceweed.com")
+                .phone("0123456789")
+                .birthDate(LocalDate.of(1985, 1, 1))
+                .enabled(true)
+                .billingAddress(new Address(1, "Rue de la Paix", "Paris", 75002, "France"))
+                .deliveryAddress(new Address(1, "Rue de la Paix", "Paris", 75002, "France"))
+                .build();
+        userService.createUser(admin);
+
+        User yapadlez = User.builder()
+                .firstname("Thomas")
+                .lastname("Noël")
+                .username("yapadlez")
+                .password("yapadlez")
+                .email("thomas.noel.31@gmail.com")
+                .phone("0680243171")
+                .birthDate(LocalDate.of(1978, 12, 28))
+                .enabled(true)
+                .billingAddress(new Address(804, "Route de Sémalens", "Puylaurens", 81700, "France"))
+                .deliveryAddress(new Address(804, "Route de Sémalens", "Puylaurens", 81700, "France"))
+                .build();
+        userService.createUser(yapadlez);
+
+        User john = User.builder()
+                .firstname("John")
+                .lastname("Doe")
+                .username("johndoe")
+                .password("password")
+                .email("john.doe@gmail.com")
+                .phone("0652451584")
+                .birthDate(LocalDate.of(1988, 7, 22))
+                .enabled(true)
+                .billingAddress(new Address(12, "Avenue des Champs-Élysées", "Paris", 75008, "France"))
+                .deliveryAddress(new Address(12, "Avenue des Champs-Élysées", "Paris", 75008, "France"))
+                .build();
+        userService.createUser(john);
+
+
+
+//        userService.createUser(new User(null, "admin", "admin", true, null));
+//        userService.createUser(new User(null, "yapadlez", "password", true, null));
+//        userService.createUser(new User(null, "johndoe", "password", true, null));
         //ajouter les rôles aux users
         userService.addRoleToUser("admin", "ADMIN");
         userService.addRoleToUser("admin", "USER");
--- src/main/java/tn/dev/usersmicroservice/entities/User.java
+++ src/main/java/tn/dev/usersmicroservice/entities/User.java
@@ -2,25 +2,54 @@ package tn.dev.usersmicroservice.entities;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder // ← permet de nommer chaque champ au moment de la création
 @Entity
-@Table(name="users")
+@Table(name = "users")
+@EntityListeners(AuditingEntityListener.class)
 public class User {
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true)
+    private String firstname;
+    private String lastname;
+
+    @Column(unique = true, nullable = false)
     private String username;
+
+    @Column(nullable = false)
     private String password;
-    private Boolean enabled;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String phone;
+    private LocalDate birthDate;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private Instant createdAt;
+
+    @LastModifiedDate
+    private Instant updatedAt;
+
+    @Column(nullable = false)
+    private Boolean enabled = true;
 
     @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     @JoinTable(
@@ -28,5 +57,53 @@ public class User {
             joinColumns = @JoinColumn(name = "user_id"),       // colonne PK de User
             inverseJoinColumns = @JoinColumn(name = "role_id") // colonne PK de Role
     )
-    private List<Role> roles;
+
+    @Builder.Default
+    private List<Role> roles = new ArrayList<>();
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name="number",    column=@Column(name="billing_number")),
+            @AttributeOverride(name="street",    column=@Column(name="billing_street")),
+            @AttributeOverride(name="city",      column=@Column(name="billing_city")),
+            @AttributeOverride(name="zipCode",   column=@Column(name="billing_zip_code")),
+            @AttributeOverride(name="state",     column=@Column(name="billing_state"))
+    })
+    private Address billingAddress;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name="number",    column=@Column(name="delivery_number")),
+            @AttributeOverride(name="street",    column=@Column(name="delivery_street")),
+            @AttributeOverride(name="city",      column=@Column(name="delivery_city")),
+            @AttributeOverride(name="zipCode",   column=@Column(name="delivery_zip_code")),
+            @AttributeOverride(name="state",     column=@Column(name="delivery_state"))
+    })
+    private Address deliveryAddress;
 }
+
+//firstname: string;
+//lastname: string;
+//username: string;
+//password: string;
+//email: string;
+//phone: string;
+//birthDate: Date;
+//createdAt: Date;
+//updatedAt: Date;
+//isActive: boolean;
+//roles: string[];
+//billing_address?: {
+//number: number;
+//street: string;
+//city: string;
+//zipCode: number;
+//state: string;
+//    };
+//delivery_address?: {
+//number: number;
+//street: string;
+//city: string;
+//zipCode: number;
+//state: string
+//    };
